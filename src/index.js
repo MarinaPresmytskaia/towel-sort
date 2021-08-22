9642054b8ac5a54e63c196179f6a2704ba5322e9
Marina <mpresmytskaia@gmail.com>
2021-08-22 17:07:13 +0300

    feat: implement towel-sort task with reduce method

--- src/index.js
+++ src/index.js
@@ -6,16 +6,9 @@ module.exports = function towelSort (matrix) {
   if(matrix === undefined) {
     return [];
   }
-
-  const arr =[];
-  let i;
-  for(i = 0; i < matrix.length; i++) {
-      for(let j = 0; j < matrix[i].length; j++) {
-      const matrixSort = i % 2 === 0
-      ? j
-      : (matrix[i].length - j -1);
-      arr.push(matrix[i][matrixSort]);
-    }
-  }
-  return arr;
+  return matrix.reduce((acc, curr, i) => {
+    i % 2 === 0 
+    ? matrix[curr]
+    : curr.sort((a,b) => b - a);
+    return acc.concat(curr)}, []);  
 }
